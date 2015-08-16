48e89d38ef5332d11a191e27a6f0ee1f3910121f
Vaden Masrani <vadmas@gmail.com>
2015-08-16 15:10:39 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+(function() {
+vis = {}
+vis.init = function(params) {
+  // code for my init function ...
+  vis.loaddata(params);
+}
+vis.loaddata = function(params) {
+  // code for loading data ...
+  vis.draw(params);
+}
+vis.draw = function(params) {
+  // code for drawing stuff ...
+}
+})();
